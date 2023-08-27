74063499d47cd6ff0197a67735cf5970ba98aa9c
Harshal-sengar <harshalsengar0123@gmail.com>
2023-08-27 12:40:23 +0530

    updated

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/ChatGPT");
+    console.log(
+      `Connected To Mongodb Database ${mongoose.connection.host}`.bgGreen.white
+    );
+  } catch (error) {
+    console.log(`Mognodb Database Error ${error}`.bgRed.white);
+  }
+};
+
+module.exports = connectDB;
